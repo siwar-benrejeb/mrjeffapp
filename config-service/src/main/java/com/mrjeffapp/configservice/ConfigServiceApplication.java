4d95e37973d8edcf427e027c83565fbca59c4e23
siwarmrjeff <siwar@mrjeffapp.com>
2017-10-14 22:11:53 +0200

    reconfigure jenkinsfil

--- config-service/src/main/java/com/mrjeffapp/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/mrjeffapp/configservice/ConfigServiceApplication.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 @SpringBootApplication
 public class ConfigServiceApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServiceApplication.class, args);
 	}
